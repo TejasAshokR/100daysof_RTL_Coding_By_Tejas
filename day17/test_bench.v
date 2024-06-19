223abed6e83168eda0ccdf93577f0f6ae9d77eb9
TejasAshokR <151009720+TejasAshokR@users.noreply.github.com>
2024-06-19 21:21:05 +0530

    Add files via upload

---
+++ day17/test_bench.v
@@ -0,0 +1,25 @@
+`timescale 1ns/1ps
+module test_bench;
+reg [2:0]i;
+wire [7:0]out;
+decoder dut(i,out);
+initial
+begin
+i=3'b000;
+#10;
+end
+always begin
+      #10 i=3'b000;
+	   #10 i=3'b001;
+		#10 i=3'b010;
+	   #10 i=3'b011;
+	   #10 i=3'b100;
+	   #10 i=3'b101;
+		#10 i=3'b110;
+		#10 i=3'b111;
+end
+initial begin
+$monitor("i=%b out=%b",i,out);
+#90 $finish;
+end
+endmodule
